f7ad0bee795add05b76bf5d6fe56f00ea16c2811
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 17:44:25 +0300

    Automatic commit at 2015-08-09 17:44:25

--- generate.py
+++ generate.py
@@ -31,7 +31,7 @@ def caverns():
     sigma = size / 4
     ex, ey = size / 2, size / 2
     density = numpy.zeros((size, size), numpy.float32)
-    for blob in range(10):
+    for blob in range(1):
         bx = random.normalvariate(ex, sigma)
         by = random.normalvariate(ey, sigma)
         sm = 10
