aeeaa885de06997e7482e36e1775a6d6e58b0cb5
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 13:17:19 +0300

    Automatic commit at 2015-08-09 13:17:19

--- basement.py
+++ basement.py
@@ -119,15 +119,18 @@ class Level:
     @staticmethod
     def generate_caverns(granularity, radius):
         import noise
+        import random
         def dnorm(x, mu, sig):
             return math.exp(-pow(x - mu, 2.) / (2 * pow(sig, 2.)))
+        xofs = random.randint(-10000, 10000)
+        yofs = random.randint(-10000, 10000)
         radius = int(radius)
         sigma = radius / 2
         level = Level()
         for x in range(-radius, radius):
             for y in range(-radius, radius):
                 r = math.sqrt(x * x + y * y)
-                n = abs(noise.pnoise2(x / granularity, y / granularity, octaves=2))
+                n = abs(noise.pnoise2((xofs + x) / granularity, (yofs + y) / granularity, octaves=2))
                 d = dnorm(r, 0, sigma) * n
                 if d > 0.09:
                     level[x, y] = Ground(level, x, y)
