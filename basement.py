8eb4dc985558c0a72e4411f35a58f39b3f9556c8
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 12:44:37 +0300

    Automatic commit at 2015-08-09 12:44:37

--- basement.py
+++ basement.py
@@ -144,8 +144,8 @@ def main(stdscr):
 
     while True:
         fov.fieldOfView(player.x, player.y, 5,
-                lambda x, y: level[x, y].see(),
-                lambda x, y: not level[x, y].is_passable)
+                lambda x, y: player.level[x, y].see(),
+                lambda x, y: not player.level[x, y].is_passable)
 
         height, width = stdscr.getmaxyx()
         stdscr.clear()
