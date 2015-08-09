78d2c487cb7bd93b248a3894a30635210cc75075
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 17:47:49 +0300

    Automatic commit at 2015-08-09 17:47:49

--- basement.py
+++ basement.py
@@ -38,4 +38,4 @@ def main(stdscr):
 if __name__ == '__main__':
     #from curses import wrapper
     #wrapper(main)
-    print(generate.caverns().view(-10, -40, 10, 40))
+    print(generate.caverns().view(-40, -10, 40, 10))
