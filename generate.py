f8f11ad1fe287bfc6d64cd7bd6be1f77b53d9fe9
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 17:27:18 +0300

    Automatic commit at 2015-08-09 17:27:18

--- generate.py
+++ generate.py
@@ -23,18 +23,5 @@ def bitmap_level(bitmap, xofs, yofs):
 
 
 def caverns(granularity, radius):
-    xofs = random.randint(-10000, 10000)
-    yofs = random.randint(-10000, 10000)
     radius = int(radius)
-    sigma = radius / 3
-    level = Level()
-    for x in range(-radius, radius):
-        for y in range(-radius, radius):
-            r = math.sqrt(x * x + y * y)
-            n = abs(noise.pnoise2((xofs + x) / granularity, (yofs + y) / granularity, octaves=3))
-            d = dnorm(r, 0, sigma) * n
-            if d > 0.09:
-                level[x, y] = Ground(level, x, y)
-    return level
-
-
+    return bitmap_level(bitmap, ex, ey)
