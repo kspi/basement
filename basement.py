5838541b12eafd50a711cdfc8453a6718f8fe72e
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 13:08:07 +0300

    Automatic commit at 2015-08-09 13:08:07

--- basement.py
+++ basement.py
@@ -155,7 +155,7 @@ def main(stdscr):
         "               #####################",
     ])
 
-    caverns = Level.generate_caverns(10)
+    caverns = Level.generate_caverns(10, 100)
 
     movement = dict(h=(-1, 0), j=(0, 1), k=(0, -1), l=(1, 0))
 
