fde3eb5268cf7f1b483de2a320fe66c5e3efc22b
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 13:06:04 +0300

    Automatic commit at 2015-08-09 13:06:04

--- basement.py
+++ basement.py
@@ -126,7 +126,7 @@ class Level:
         for x in range(-extent, extent):
             for y in range(-extent, extent):
                 r = math.sqrt(x * x + y * y)
-                n = noise.pnoise2(x / extent, y / extent)
+                n = noise.pnoise2(x / extent, y / extent) * 0.5 + 0.5
                 print(n)
                 d = dnorm(r, 0, sigma) * n
                 if d > 0.001:
