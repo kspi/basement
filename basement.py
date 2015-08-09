a8cf39f68f6e1688745c656629fc02161e24dd95
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-09 13:02:47 +0300

    Automatic commit at 2015-08-09 13:02:47

--- basement.py
+++ basement.py
@@ -121,7 +121,7 @@ class Level:
         import noise
         def dnorm(x, mu, sig):
             return math.exp(-pow(x - mu, 2.) / (2 * pow(sig, 2.)))
-        extent = sigma * 3
+        extent = sigma * 2
         level = Level()
         for x in range(-extent, extent):
             for y in range(-extent, extent):
