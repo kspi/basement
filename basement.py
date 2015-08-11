43d1831d55ab8a8205b8ede7e3683bd5e6da85e0
Kiprianas Spiridonovas <k.spiridonovas@gmail.com>
2015-08-11 19:05:46 +0300

    Automatic commit at 2015-08-11 19:05:46

--- basement.py
+++ basement.py
@@ -26,7 +26,7 @@ class Basement:
         self.player = Player(caverns[0, 0])
 
     def act(self):
-        return False
+        pass
 
     def ask(self, question):
         prevcursor = self.stdscr.getyx()
